924b19be07aa8d9400839125c77cfc5ed06c7af7
justine wright <wright.justine21@gmail.com>
2021-10-04 17:46:43 +0200

    Renames Tests

--- project-squirrel-dragonTests/TempTests.swift
+++ project-squirrel-dragonTests/TempTests.swift
@@ -7,7 +7,7 @@
 
 import XCTest
 
-class project_squirrel_dragonTests: XCTestCase {
+class TempTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
