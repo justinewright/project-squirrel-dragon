91cba6c631e5e4f4ac044faff6f613419ffabeac
justine wright <wright.justine21@gmail.com>
2021-10-04 12:12:28 +0200

    adds test folder

---
+++ project-squirrel-dragonTests/project_squirrel_dragonTests.swift
@@ -0,0 +1,32 @@
+//
+//  project_squirrel_dragonTests.swift
+//  project-squirrel-dragonTests
+//
+//  Created by Justine Wright on 2021/10/04.
+//
+
+import XCTest
+
+class project_squirrel_dragonTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
