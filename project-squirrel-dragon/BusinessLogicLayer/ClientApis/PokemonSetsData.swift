635df6f1343a288c745e87dfbdc4efa148381200
justine wright <wright.justine21@gmail.com>
2021-10-06 09:39:47 +0200

    Adds PokemonSetsData

--- project-squirrel-dragon/BusinessLogicLayer/ClientApis/PokemonSetsData.swift
+++ project-squirrel-dragon/BusinessLogicLayer/ClientApis/PokemonSetsData.swift
@@ -8,13 +8,17 @@
 import Foundation
 
 struct PokemonSetsData: Codable {
+    let data: [SetsData]
+}
+
+struct SetsData: Codable {
     let id: String
     let name: String
     let series: String
     let printedTotal: Int
     let total: Int
     let releaseDate: String
-    let updatedAt: String //yyyy/mm//dd hh:mm:ss
+    let updatedAt: String // yyyy/mm//dd hh:mm:ss
     let images: Images
 }
 
