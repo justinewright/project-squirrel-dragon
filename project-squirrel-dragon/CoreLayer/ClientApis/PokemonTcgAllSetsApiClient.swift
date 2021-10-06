df82cf96200ae603bd9f2e15e65beada71071a6b
justine wright <wright.justine21@gmail.com>
2021-10-06 13:36:53 +0200

    Addresses PR Change Requests

--- project-squirrel-dragon/CoreLayer/ClientApis/PokemonTcgAllSetsApiClient.swift
+++ project-squirrel-dragon/CoreLayer/ClientApis/PokemonTcgAllSetsApiClient.swift
@@ -6,10 +6,8 @@
 //
 
 import Foundation
-import Combine
 
 class PokemonTcgAllSetsApiClient: PokemonTcgApiClientProtocol {
-    private var cancellables = Set<AnyCancellable>()
     private let APIkey = "9429b091-1eb8-4db3-8e72-50c2ca7bfede"
     private let requestHeaderField = "X-Api-Key"
     private let apiSetsUrlString = "https://api.pokemontcg.io/v2/sets"
@@ -20,22 +18,22 @@ class PokemonTcgAllSetsApiClient: PokemonTcgApiClientProtocol {
         // adding api key to header
         var request = URLRequest(url: url)
         request.setValue(APIkey, forHTTPHeaderField: requestHeaderField)
-
-        URLSession
-            .DataTaskPublisher(request: request,
-                               session: URLSession(configuration: .default))
-            .retry(3)
-            .tryMap() { element -> Data in
-                guard let httpResponse = element.response as? HTTPURLResponse,
-                      httpResponse.statusCode == 200 else {
-                          handler(.failure(URLError(.badServerResponse)))
-                          throw URLError(.badServerResponse)
-                      }
-                return element.data
+        
+        let session = URLSession(configuration: .default)
+        let task =  session.dataTask(with: request) { data, response, _ in
+            guard let httpResponse = response as? HTTPURLResponse,
+                  httpResponse.statusCode == 200 else {
+                      handler(.failure(URLError(.badServerResponse)))
+                      return
+                  }
+            do {
+                let decodedData = try JSONDecoder().decode(PokemonSetsData.self, from: data!)
+                handler(.success(decodedData))
+            } catch {
+                handler(.failure(URLError(.badServerResponse)))
             }
-            .decode(type: PokemonSetsData.self, decoder: JSONDecoder())
-            .sink(receiveCompletion: { print("\($0)")
-            }, receiveValue: {  handler(.success($0)) })
-            .store(in: &cancellables)
+        }
+        task.resume()
+
     }
 }
