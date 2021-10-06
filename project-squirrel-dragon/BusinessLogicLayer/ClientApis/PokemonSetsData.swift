827e702cf20f810ec2415aa3730a85d17c8545a0
justine wright <wright.justine21@gmail.com>
2021-10-06 07:55:01 +0200

    Adds Pokemon Sets Data

---
+++ project-squirrel-dragon/BusinessLogicLayer/ClientApis/PokemonSetsData.swift
@@ -0,0 +1,24 @@
+//
+//  PokemonSetsData.swift
+//  project-squirrel-dragon
+//
+//  Created by Justine Wright on 2021/10/06.
+//
+
+import Foundation
+
+struct PokemonSetsData: Codable {
+    let id: String
+    let name: String
+    let series: String
+    let printedTotal: Int
+    let total: Int
+    let releaseDate: String
+    let updatedAt: String //yyyy/mm//dd hh:mm:ss
+    let images: Images
+}
+
+struct Images: Codable {
+    let symbol: String
+    let logo: String
+}
