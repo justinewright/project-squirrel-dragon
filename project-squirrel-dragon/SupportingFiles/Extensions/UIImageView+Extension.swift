3e9fbc7f2a30fada3ff8b2db5284b265f267fe80
justine wright <wright.justine21@gmail.com>
2021-10-07 18:38:18 +0200

    Adds extension

---
+++ project-squirrel-dragon/SupportingFiles/Extensions/UIImageView+Extension.swift
@@ -0,0 +1,22 @@
+//
+//  UIImageView+Extension.swift
+//  project-squirrel-dragon
+//
+//  Created by Justine Wright on 2021/10/07.
+//
+
+import UIKit
+
+extension UIImageView {
+    func load(url: URL) {
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.image = image
+                    }
+                }
+            }
+        }
+    }
+}
