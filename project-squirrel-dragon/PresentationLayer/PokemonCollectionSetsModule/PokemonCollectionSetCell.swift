0c3e907c644b229910f65c5d50ea6f4490ea7a96
justine wright <wright.justine21@gmail.com>
2021-10-07 18:37:49 +0200

    Adds module's cell

---
+++ project-squirrel-dragon/PresentationLayer/PokemonCollectionSetsModule/PokemonCollectionSetCell.swift
@@ -0,0 +1,44 @@
+//
+//  CollectionViewCell.swift
+//  project-squirrel-dragon
+//
+//  Created by Justine Wright on 2021/10/07.
+//
+
+import UIKit
+
+class PokemonCollectionSetCell: UICollectionViewCell {
+
+    // MARK: - Properties
+    @IBOutlet private weak var pokemonCollectionSetImageView: UIImageView!
+    private var urlString: String = ""
+    private let cornerRadius: CGFloat = 10
+    private let borderWidth: CGFloat = 1
+    private let borderColour: CGColor = UIColor(ciColor: .white).cgColor
+
+    // MARK: - Initialization
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        applyStyle()
+        layer.cornerRadius = 10
+        layer.borderWidth = 1
+        layer.borderColor = UIColor(ciColor: .white).cgColor
+    }
+
+    func configure(with pokemonCollectionSet: PokemonCollectionSet) {
+        urlString = pokemonCollectionSet.imageLogo
+        configurePokemonCollectionSetImageView()
+    }
+
+    private func configurePokemonCollectionSetImageView() {
+        guard let url = URL(string: urlString) else { return }
+        pokemonCollectionSetImageView.load(url: url)
+    }
+
+    private func applyStyle() {
+        layer.cornerRadius = cornerRadius
+        layer.borderWidth = borderWidth
+        layer.borderColor = borderColour
+    }
+
+}
--- project-squirrel-dragon/PresentationLayer/ViewController.swift
+++ project-squirrel-dragon/PresentationLayer/ViewController.swift
@@ -9,11 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var child = UIViewController()
     override func viewDidLoad() {
         super.viewDidLoad()
+        child = PokemonCollectionSetsModuleBuilder.build()
+        addChild( child)
+        view.addSubview(child.view)
+        child.didMove(toParent: self)
         // Do any additional setup after loading the view.
     }
 
-
 }
-
