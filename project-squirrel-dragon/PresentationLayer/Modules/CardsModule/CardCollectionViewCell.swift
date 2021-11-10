90e44cc645b9d0cb0e4e67f5f0690e66a4abd4ed
justine wright <wright.justine21@gmail.com>
2021-11-10 19:08:20 +0200

    Adds Card Module's View

--- project-squirrel-dragon/PresentationLayer/Modules/CardsModule/CardCollectionViewCell.swift
+++ project-squirrel-dragon/PresentationLayer/Modules/CardsModule/CardCollectionViewCell.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class CardCollectionViewCell: UICollectionViewCell {
 
-    @IBOutlet weak var cardImageView: UIImageView!
+    @IBOutlet private weak var cardImageView: UIImageView!
     @IBOutlet weak var ownedImageView: UIImageView!
     @IBOutlet weak var rarityImageView: UIImageView!
     @IBOutlet weak var collectedNumberLabel: UILabel!
@@ -19,4 +19,35 @@ class CardCollectionViewCell: UICollectionViewCell {
         // Initialization code
     }
 
+    func applyStyle() {
+        backgroundView?.alpha = 0.5
+    }
+
+    func configure(with collectableCard: CollectableCard) {
+        configureCollectedNumberLabel(withNumberText:  "\(collectableCard.userCard.collectedNumber)")
+        configureOwnedImageView(setOwned: collectableCard.userCard.collectedNumber > 0)
+        configureCardImageView(withURLString: collectableCard.cardImages.small)
+    }
+
+    private func configureCollectedNumberLabel(withNumberText numberText: String) {
+        collectedNumberLabel.text = numberText
+
+    }
+    private func configureCardImageView(withURLString urlString: String) {
+        guard let url = URL(string: urlString) else {return}
+        cardImageView.load(url: url)
+    }
+
+    private func configureOwnedImageView(setOwned: Bool) {
+        let imageName = setOwned ? "checkmark.circle.fill" : "circle"
+        let image = UIImage(systemName: imageName)
+        ownedImageView.image = image
+    }
+
+    override var isSelected: Bool {
+        willSet(newValue) {
+            selectedBackgroundView?.alpha = 1
+            configureOwnedImageView(setOwned: newValue)
+        }
+    }
 }
---
+++ project-squirrel-dragon/PresentationLayer/Modules/CardsModule/CardsModuleBuilder.swift
@@ -0,0 +1,20 @@
+//
+//  CardsModuleBuilder.swift
+//  project-squirrel-dragon
+//
+//  Created by Justine Wright on 2021/11/10.
+//
+
+import Foundation
+
+class CardsModuleBuilder {
+
+    static func build(usingNavigationFactory factory: NavigationFactory, andPokemonSet set:PokemonCollectionSet) -> UIViewController {
+        let storyboard = UIStoryboard.init(name: "Cards", bundle: nil)
+        let view = storyboard.instantiateViewController(withIdentifier: "Cards")
+        view.title = set.name + "'s Cards"
+        view.navigationItem.largeTitleDisplayMode = .never
+        return view
+    }
+
+}
