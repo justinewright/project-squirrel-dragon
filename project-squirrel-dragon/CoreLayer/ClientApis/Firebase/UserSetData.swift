6cc062df3596652ce4729d04de309328bea1520c
justine wright <wright.justine21@gmail.com>
2021-11-23 16:19:56 +0200

    Cleans up code

--- project-squirrel-dragon/CoreLayer/ClientApis/Firebase/UserSetData.swift
+++ project-squirrel-dragon/CoreLayer/ClientApis/Firebase/UserSetData.swift
@@ -21,48 +21,26 @@ struct FirebaseData<T:Codable>: Codable {
 struct UserSetData: Codable {
     let id: String
     let collectedCards: Int
-//    var cardData: [FirebaseCollectedCardData] = []
 
     init(id: String, collectedCards: Int) {
         self.id = id
         self.collectedCards = collectedCards
-//        self.cardData = cardData
     }
 
     init(userSet: UserSet) {
         self.id = userSet.id
         self.collectedCards = userSet.cardsCollected
-//        self.cardData = []
     }
 
     func toAnyObject() -> Any {
         return [
             "id": id,
             "collectedCards": collectedCards
-//            "cardData": cardData
         ]
     }
 
     enum CodingKeys: String, CodingKey {
             case id = "id"
             case collectedCards = "collectedCards"
-//            case cardData = "cardData"
-        }
-}
-
-struct FirebaseCollectedCardData: Codable {
-    let rarity: String
-    let collectedAmount: Int
-
-    func toAnyObject() -> Any {
-            return [
-                "rarity": rarity,
-                "collectedAmount": collectedAmount
-            ]
-    }
-    
-    enum CodingKeys: String, CodingKey {
-            case rarity = "rarity"
-            case collectedAmount = "collectedAmount"
         }
 }
